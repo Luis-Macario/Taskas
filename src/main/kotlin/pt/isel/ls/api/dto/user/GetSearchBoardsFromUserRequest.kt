fc69370d4edf22cc45c05483d0701f97f6a247c3
RicardoPinto26 <a47673@alunos.isel.pt>
2023-05-10 04:25:49 +0100

    Deletes an unused DTO

--- src/main/kotlin/pt/isel/ls/api/dto/user/GetSearchBoardsFromUserRequest.kt
+++
@@ -1,7 +0,0 @@
-package pt.isel.ls.api.dto.user
-
-import kotlinx.serialization.Serializable
-import pt.isel.ls.api.dto.board.SimpleBoardDTO
-
-@Serializable
-data class GetSearchBoardsFromUserRequest(val boardsName: String)
